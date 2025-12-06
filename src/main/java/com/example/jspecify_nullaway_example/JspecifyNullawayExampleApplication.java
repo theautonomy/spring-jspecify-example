8eee97d37dfa98065e611b7dda5bd082099a4337
Wei Li <weili.mail@gmail.com>
2025-12-06 10:57:18 -0500

    Check in

---
+++ src/main/java/com/example/jspecify_nullaway_example/JspecifyNullawayExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.jspecify_nullaway_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspecifyNullawayExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JspecifyNullawayExampleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/jspecify_nullaway_example/JspecifyNullawayExampleApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.jspecify_nullaway_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspecifyNullawayExampleApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
